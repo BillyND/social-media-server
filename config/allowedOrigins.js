285daf57584daa32a82bf75afddfb354291e008a
NDLong <longnd1@bravebits.vn>
2023-08-12 17:53:51 +0700

    update allowedOrigins, add pm2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:4200",
   "https://realword-clone.vercel.app/",
+  "https://realworld-clone-billynd.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
